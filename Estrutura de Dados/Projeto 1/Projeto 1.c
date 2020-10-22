adfe18422e6445a95432e92d5eb27d03383b925c
Pedro Henrique Pistori Costa <pedropistori@gmail.com>
2020-10-21 22:40:31 -0400

    Atualizacao Projeto 1
    +Adicionado Função para Visualizar a lista de produtos;
    +Adicionado Complemento, para a Função anterior, de Visualização detalhada por ID;
    +Corrigido erro da ID permanecer sempre 1;
    +Adicionado Titulo com Identificação e Referencias, conforme enunciado do Projeto;

--- Estrutura de Dados/Projeto 1/Projeto 1.c
+++ Estrutura de Dados/Projeto 1/Projeto 1.c
@@ -1,3 +1,16 @@
+/*
+ALUNO: PEDRO HENRIQUE PISTORI COSTA
+UF: UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
+ESTRUTURA DE DADOS 2020.1
+TEMA: SISTEMA DE MERCADO
+PROJETO 1
+
+REFERENCIAS: 
+    Youtube.com/user/italogross -> PONTEIROS
+    Youtube.com/user/canaldofree -> LISTAS DINAMICAS ENCADEADAS
+    Youtube.com/user/progdescomplicada -> LISTAS DINAMICAS ENCADEADAS
+*/
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
@@ -10,7 +23,7 @@ typedef struct itemProduto
     char desc[100]; //DESCRIÇÃO DO PRODUTO
     double preco;
     int qntd;
-    char unidade[7]; //UNIDADE DE MEDIDA
+    char unidade[3]; //UNIDADE DE MEDIDA (KG,L,UND,CX)
 }ItemProduto;
 
 typedef struct elementoProduto
@@ -29,7 +42,11 @@ void title(char opcao[30]);
 int menu();
 ListaProduto* criarListaProduto();
 ItemProduto preencherItem(ItemProduto item);
+int itemUnidade();
 void adicionarItem(ListaProduto *lista, ItemProduto item);
+bool listaVazia(ListaProduto *lista);
+void visualizarLista(ListaProduto *lista);
+void visualizarListaDetalhes(ListaProduto *lista);
 
 int main(int argc, char const *argv[])
 {
@@ -49,7 +66,8 @@ int main(int argc, char const *argv[])
         case 1: // ADICIONAR PRODUTO
             system("cls");
             title("Adicionar Produto");
-
+            itemProduto.id++; //CONTROLE DA ID DO PRODUTO!
+            adicionarItem(listaProduto,itemProduto);
             break;
         case 2: // REMOVER PRODUTO
             system("cls");
@@ -58,7 +76,7 @@ int main(int argc, char const *argv[])
         case 3: // VISUALIZAR PRODUTOS
             system("cls");
             title("Visualizar Produto");
-
+            visualizarLista(listaProduto);
             pausar();
             break;
         case 4: // FINALIZAR PROGRAMA
@@ -68,7 +86,6 @@ int main(int argc, char const *argv[])
             printf("Opcao nao reconhecida, favor tente novamente..\n");
             break;
         }
-
     }
     return 0;
 }
@@ -82,7 +99,7 @@ void pausar(){ //FUNÇÃO PARA PAUSAR O PROGRAMA
 
 void title(char opcao[30]){
     printf("-------------------------------\n");
-    printf("------Mercadinho do Pedro------\n");
+    printf("----- Mercadinho do Pedro -----\n");
     printf("-------------------------------\n");
 
     if ( !(strcmp(opcao,"") == 0)){
@@ -115,8 +132,8 @@ ListaProduto* criarListaProduto(){
 
 ItemProduto preencherItem(ItemProduto item){
     int op;
-    item.id++;
-    printf("Código do produto: %d\n\n", item.id);
+    
+    printf("Codigo do produto: %d\n\n", item.id);
     
     printf("Insira o nome do produto: ");
     scanf("%s",&item.nome);
@@ -127,7 +144,7 @@ ItemProduto preencherItem(ItemProduto item){
     printf("Insira a Quantidade em estoque: ");
     scanf("%d",&item.qntd);
     fflush(stdin);
-    printf("Qual a unidade de medida?\n");
+    printf("\nQual a unidade de medida?\n");
     do{
         op = itemUnidade();
         if (op<1 || op>4)
@@ -138,16 +155,16 @@ ItemProduto preencherItem(ItemProduto item){
     switch (op)
     {
     case 1:
-        strcpy(item.unidade,"Kg"); //QUILOGRAMAS
+        strcpy(item.unidade,"KG"); //QUILOGRAMAS
         break;
     case 2:
-        strcpy(item.unidade,"LT"); //LITROS
+        strcpy(item.unidade,"L"); //LITROS
         break;
     case 3:
-        strcpy(item.unidade,"Unidades"); //UNIDADES
+        strcpy(item.unidade,"Und"); //UNIDADES
         break;
     case 4:
-        strcpy(item.unidade,"Caixas"); //CAIXAS
+        strcpy(item.unidade,"Cx"); //CAIXAS
         break;
     }
     printf("Insira o preco do produto: ");
@@ -158,18 +175,101 @@ ItemProduto preencherItem(ItemProduto item){
 
 int itemUnidade(){
     int op;
-    printf("OPCAO 1 - KG\n");
-    printf("OPCAO 2 - LT\n");
-    printf("OPCAO 3 - Unidades\n");
-    printf("OPCAO 4 - Caixas\n\n");
+    printf("OPCAO 1 - QUILOGRAMAS-(KG)\n");
+    printf("OPCAO 2 - LITROS - - -(L)\n");
+    printf("OPCAO 3 - UNIDADES - -(UND)\n");
+    printf("OPCAO 4 - CAIXAS - - -(CX)\n\n");
     printf("Insira uma opcao: ");
     scanf("%d",&op);
     fflush(stdin);
-
     return op;
 }
 
 void adicionarItem(ListaProduto *lista, ItemProduto item){
-    preencherItem(item);
+    ElementoProduto *elemento = (ElementoProduto*) malloc(sizeof(ElementoProduto));
+    item = preencherItem(item);
+    elemento->itemProduto = item;
     
+    elemento->proximo = NULL;
+    if (lista->tamanho == 0)
+    {
+        lista->inicio = elemento;
+    }else
+    {
+        lista->final->proximo = elemento; // O ELEMENTO QUE SE ENCONTRAVA NO FINAL, VAI APONTAR PARA O ELEMENTO RECEM CRIADO(PROXIMO). 
+    }
+    lista->final = elemento; //O FINAL VAI APONTAR PARA O ELEMENTO RECEM CRIADO, QUE SE ENCONTRA NO FINAL DA LISTA.
+    lista->tamanho++;
+}
+
+bool listaVazia(ListaProduto *lista){
+    if (lista->tamanho == 0)
+    {
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+
+void visualizarLista(ListaProduto *lista){
+    if(listaVazia(lista)){
+        printf("A Lista se encontra vazia!\n\n");
+        return;
+    }
+    int op;
+    ElementoProduto *apontador = lista->inicio;
+    while (apontador != NULL)
+    {   
+        printf("ID: %d\n", apontador->itemProduto.id);
+        printf("NOME: %s\n",  apontador->itemProduto.nome);
+        printf("QNTD: %d%s\n", apontador->itemProduto.qntd, apontador->itemProduto.unidade);
+        printf("____________________\n");
+        apontador = apontador->proximo;
+    }
+
+    printf("\nDeseja ver algum item Detalhadamente? (1 - SIM | 2 - NAO)\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d",&op);
+    fflush(stdin);
+    if (op == 1){
+        visualizarListaDetalhes(lista);
+    }
+    printf("\n");
+}
+
+void visualizarListaDetalhes(ListaProduto *lista){
+    
+    int idPesquisa;
+    int count = 0;
+    ElementoProduto *apontador = lista->inicio;
+
+    printf("Insira o ID do item para detalhar: ");
+    scanf("%d", &idPesquisa);
+    fflush(stdin);
+    system("cls");
+
+    title("VISUALIZAR DETALHES");
+
+    while(apontador != NULL)
+    {
+        if (apontador->itemProduto.id == idPesquisa)
+        {
+            printf("____________________\n");
+            printf("ID: %d\n", apontador->itemProduto.id);
+            printf("NOME: %s\n", apontador->itemProduto.nome);
+            printf("DESCRICAO: %s\n", apontador->itemProduto.desc);
+            printf("QUANTIDADE EM ESTOQUE: %d %s\n", apontador->itemProduto.qntd, apontador->itemProduto.unidade);
+            printf("PRECO: R$ %.2f\n", apontador->itemProduto.preco);
+            printf("____________________\n\n");
+        }else
+        {
+            count++;
+        }
+      apontador = apontador->proximo;
+    }
+    if (count == lista->tamanho)
+    {
+        printf("NAO FOI ENCONTRADO NENHUM ITEM COM O CODIGO: %d!\n\n", idPesquisa);
+    }
 }
